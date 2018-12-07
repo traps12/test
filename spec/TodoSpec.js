a9b690dbe282bf9e2bda7469fde5a98aaa3c3404
traps12 <trupti.garotkar@gmail.com>
2018-12-07 15:31:58 +0530

    Added new code

--- spec/TodoSpec.js
+++ spec/TodoSpec.js
@@ -8,20 +8,17 @@ var calculationHelper = new CalculationHelper();
 
 describe("Calculation Addition Page", function() {
     it("should able to add two valid number", function() {
- 
-     
       calculationHelper.get();
       calculationPage.setFirstField(number1);
       calculationPage.setSecondField(number2);
-        calculationPage.buttonClick();
-
-    //  calculationPage.setSecondField(number2);
+      calculationPage.buttonClick();
       expect(calculationPage.gettotalsum()).toEqual(calculationHelper.addition(number1, number2));
- 
-  
-   
-        
     });
 
- 
+    it("should not able to add two invalid number", function() {
+      calculationPage.setFirstField('d');
+      calculationPage.setSecondField('f');
+      calculationPage.buttonClick();
+      expect(calculationPage.gettotalsum()).toEqual('NaN');
+    });
 });
