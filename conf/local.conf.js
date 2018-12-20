78b087d59ef44eaeb5dc7eeae3a3a70806f7fa8d
traps12 <45550640+traps12@users.noreply.github.com>
2018-12-20 18:00:31 +0530

    Update local.conf.js

--- conf/local.conf.js
+++ conf/local.conf.js
@@ -1,14 +1,21 @@
 var browserstack = require('browserstack-local');
-
 const config = require('config'); 
-var url = config.get("web.url")
+var url = config.get("web.url");
+let bs_local = new browserstack.Local();
+var globalTunnel = require('global-tunnel-ng');
+globalTunnel.initialize({
+  host: 'internetpu',
+  port: 8085,
+
+});
 
 exports.config = {
-    specs: ["../spec/TodoSpec.js"],
-    seleniumAddress: 'http://hub-cloud.browserstack.com/wd/hub',
-
-    jasmineNodeOpts: {
+   specs: ["../spec/TodoSpec.js"],
+   //seleniumAddress: 'http://hub-cloud.browserstack.com/wd/hub',
+  // webDriverProxy:'http://internetpu:8085',
+   jasmineNodeOpts: {
         showColors: true, // Use colors in the command line report.
+        defaultTimeoutInterval: 1000000,
     },
     params: {
         env:url,
@@ -17,30 +24,33 @@ exports.config = {
         'browserstack.user': 'truptigarotkar1',
         'browserstack.key': 'FotasUZMqVHy848ZhqxE',
         'browserstack.local': true,
-        'browserName': 'chrome'
+        'browserstack.debug': true,
+        'browserName': 'firefox',
       },
-     
-beforeLaunch: function(){
-    console.log("Connecting local");
-    return new Promise(function(resolve, reject){
-      exports.bs_local = new browserstack.Local();
-      exports.bs_local.start({'key': exports.config.capabilities['browserstack.key'] }, function(error) {
-        if (error) return reject(error);
+      
+ // Code to start browserstack local before start of test
+ beforeLaunch: function(){
+  console.log("Connecting local");
+  return new Promise(function(resolve, reject){
+    bs_local.start({'key': 'FotasUZMqVHy848ZhqxE','proxy-host': 'internetpu', 'proxy-port': '8085' }, function(error) {
+    if (error) {
+        console.log(error)
+        return reject(error);}
         console.log('Connected. Now testing...');
         resolve();
-      });
     });
-  },
+  });
+},
 
-  // Code to stop browserstack local after end of test
-  afterLaunch: function(){
-    return new Promise(function(resolve, reject){
-    exports.bs_local.stop(resolve);
-    });
-  }
+
+ //Code to stop browserstack local after end of test
+ afterLaunch: function(){
+  return new Promise(function(resolve, reject){
+   globalTunnel.end();
+   bs_local.stop(resolve);
+  });
+}
 }; 
-
-
 
 
   
