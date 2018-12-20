ab3801357e135008f64305c643a1df140a8e03ae
traps12 <45550640+traps12@users.noreply.github.com>
2018-12-20 18:01:16 +0530

    Create single.conf.js

---
+++ conf/single.conf.js
@@ -0,0 +1,38 @@
+var browserstack = require('browserstack-local');
+const config = require('config'); 
+var url = config.get("web.url");
+var globalTunnel = require('global-tunnel-ng');
+globalTunnel.initialize({
+  host: 'internetpu',
+  port: 8085,
+
+});
+
+exports.config = {
+   specs: ["../spec/TodoSpec.js"],
+   seleniumAddress: 'http://hub-cloud.browserstack.com/wd/hub',
+//    webDriverProxy:'http://internetpu:8085',
+   jasmineNodeOpts: {
+        showColors: true, // Use colors in the command line report.
+        defaultTimeoutInterval: 1000000,
+    },
+    params: {
+        env:url,
+    },
+    capabilities: {
+        'browserstack.user': 'truptigarotkar1',
+        'browserstack.key': 'FotasUZMqVHy848ZhqxE',
+        'browserstack.debug': true,
+        'browserName': 'Chrome',
+      },
+
+//Code to stop browserstack local after end of test
+ afterLaunch: function(){
+  return new Promise(function(resolve, reject){
+   globalTunnel.end();
+  });
+}
+}; 
+
+
+  
