c9bb1ff9a8e1c1ff422feec0bee5ebfaf8c0286f
traps12 <trupti.garotkar@gmail.com>
2018-12-31 13:53:18 +0530

    Reporting Added

--- config/config_exec/common.conf.js
+++ config/config_exec/common.conf.js
@@ -2,6 +2,8 @@ var Jasmine2HtmlReporter = require("protractor-jasmine2-html-reporter");
 process.env["NODE_CONFIG_DIR"] ="./config/config_env";
 let config = require('config'); 
 var url = config.get("web.url")
+var HtmlReporter = require('protractor-beautiful-reporter');
+
 
 module.exports = {
     specs: [],
@@ -12,12 +14,18 @@ module.exports = {
         env:url,
     },
    framework: "jasmine2" ,
-    onPrepare: function() {
-        jasmine.getEnv().addReporter(
-          new Jasmine2HtmlReporter({
-            savePath: "target/screenshots"
-          })
-        );
-     },
+    // onPrepare: function() {
+    //     jasmine.getEnv().addReporter(
+    //       new Jasmine2HtmlReporter({
+    //         savePath: "target/screenshots"
+    //       })
+    //     );
+    //  },
+     onPrepare: function() {
+        // Add a screenshot reporter and store screenshots to `/tmp/screenshots`:
+        jasmine.getEnv().addReporter(new HtmlReporter({
+           baseDirectory: 'tmp/screenshots'
+        }).getJasmine2Reporter());
+     }
    
 };
